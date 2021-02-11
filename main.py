270a5d163addf619e3a49c4325812b8d0c6f792c
Emily <linemily97@gmail.com>
2021-02-10 21:20:50 -0800

    print header

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+def print_header():
+  print('''
+
+  ____   ___   ____                                            
+  |    \ /  _] /    |                                           
+  |  o  )  [_ |   __|                                           
+  |   _/    _]|  |  |                                           
+  |  | |   [_ |  |_ |                                           
+  |  | |     ||     |                                           
+  |__| |_____||___,_|                                           
+                                                                
+    _____  ___   _      ____  ______   ____  ____  ____     ___ 
+  / ___/ /   \ | |    |    ||      | /    ||    ||    \   /  _]
+  (   \_ |     || |     |  | |      ||  o  | |  | |  D  ) /  [_ 
+  \__  ||  O  || |___  |  | |_|  |_||     | |  | |    / |    _]
+  /  \ ||     ||     | |  |   |  |  |  _  | |  | |    \ |   [_ 
+  \    ||     ||     | |  |   |  |  |  |  | |  | |  .  \|     |
+    \___| \___/ |_____||____|  |__|  |__|__||____||__|\_||_____|
+                                                                
+  '''
+  )
+  print("How to play: \n \nAt the beginning, all the holes are filled except for the middle one. \nThe goal is to only have one remaining peg in the center hole. \nTo remove a peg, you can jump over it.")
+
+# https://www.patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+
+print_header()
